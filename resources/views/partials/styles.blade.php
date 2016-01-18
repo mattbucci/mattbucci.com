68173cb6c9fcfd996d17d6689466cc2e82bd47f8
Matthew Bucci <buccim2@gene.com>
2016-01-18 00:34:14 -0800

    settle on fonts for the site

--- resources/views/partials/styles.blade.php
+++ resources/views/partials/styles.blade.php
@@ -1,3 +1,5 @@
+<link href='https://fonts.googleapis.com/css?family=PT+Serif:400,400italic,700italic,700' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="/fonts/bebas.css">
 <link rel="stylesheet" href="/css/styles.css">
 
 @yield('styles')
